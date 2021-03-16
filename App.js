44ca24ae452ae6390b8427ad9225d92d804592e1
Luong Minh <virutphalangxa@gmail.com>
2021-03-16 10:49:29 +0700

    bai tap 2

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
     return (
       <SafeAreaView 
         style={styles.container}>
-        <View 
+        {/* <View 
           style={{
             flex: 1,
             flexDirection: 'column'
@@ -42,6 +42,27 @@ export default class App extends Component {
           <View style={{flex: 1, backgroundColor: 'blue', justifyContent: 'center', alignItems: 'center'}}>
             <Text>D</Text>
           </View>
+        </View>  */}
+        <View style={{backgroundColor: 'red',flex: 2,justifyContent: 'center', alignItems: 'center',borderWidth: 4, borderColor: '#a9a9a9'}}>
+          <Text style={{color: 'while'}}>1</Text>
+        </View>
+        <View style={{flex: 8, flexDirection: 'row'}}>
+          <View style={{backgroundColor: 'yellow', flex: 3,justifyContent: 'center', alignItems: 'center',borderWidth: 4, borderColor: '#a9a9a9'}}>
+            <Text>2</Text>
+          </View>
+          <View style={{flex: 6,flexDirection: 'column'}}>
+            <View style={{backgroundColor: 'blue', flex: 4,justifyContent: 'center', alignItems: 'center',borderWidth: 4, borderColor: '#a9a9a9'}}>
+              <Text>3</Text>
+            </View>
+            <View style={{flexDirection: 'row', flex: 2}}>
+              <View style={{backgroundColor: 'pink', flex: 1.1,justifyContent: 'center', alignItems: 'center',borderWidth: 4, borderColor: '#a9a9a9'}}>
+                <Text>4</Text>
+              </View>
+              <View style={{backgroundColor: 'purple', flex: 0.9,justifyContent: 'center', alignItems: 'center',borderWidth: 4, borderColor: '#a9a9a9'}}>
+                <Text>5</Text>
+              </View>
+            </View>
+          </View>
         </View>
       </SafeAreaView>
     );
@@ -50,8 +71,8 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    flexDirection: 'column',
+    flex: 10, 
+    flexDirection: 'column',    
     alignItems: 'stretch'
   }
 })
