95ef3cdea0f332d10b12e45b0653a530665e17d2
Luong Minh <virutphalangxa@gmail.com>
2021-03-17 20:50:37 +0700

    xuat 1 mang

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -3,18 +3,26 @@ import { SafeAreaView, Text, View } from 'react-native'
 
 export default class MainScreen extends Component {
 
-    shouldShowVn(isMemorized, vn)
-    {
-        if(isMemorized)
-        {
-            return '----'
-        }else{
-            return vn
-        }
-    }
+    // shouldShowVn(isMemorized, vn)
+    // {
+    //     if(isMemorized)
+    //     {
+    //         return '----'
+    //     }else{
+    //         return vn
+    //     }
+    // }
     render() {
         // destructuring
-        const {en, vn, isMemorized} = {en: 'One', vn: 'Mot', isMemorized: true}
+        // const {en, vn, isMemorized} = {en: 'One', vn: 'Mot', isMemorized: true}
+
+        const words = [
+            {id:1, en: 'one', vn: 'Mot'},
+            {id:2, en: 'two', vn: 'Hai'},
+            {id:3, en: 'three', vn: 'Ba'}
+        ]
+
+        const arrNames = ['teo','ti','tun','tuan']
         return (
             <SafeAreaView 
             style={{
@@ -22,8 +30,21 @@ export default class MainScreen extends Component {
                 flexDirection: 'row', 
                 justifyContent: 'space-evenly'
             }}>
-                <Text style={{fontSize: 30}}> {en} </Text>
-                <Text style={{fontSize: 30}}> {this.shouldShowVn(isMemorized,vn)} </Text>
+                {/* <Text style={{fontSize: 30}}> {en} </Text>
+                <Text style={{fontSize: 30}}> {isMemorized ? '----': vn} </Text> */}
+            {
+                [
+                <Text>{arrNames[0]}</Text>,
+                <Text>{arrNames[1]}</Text>,
+                <Text>{arrNames[2]}</Text>,
+                <Text>{arrNames[3]}</Text>
+                ]
+            }
+            {/* {
+                arrNames.map((name) =>{
+                    return <Text>{name}</Text>
+                })
+            } */}
             </SafeAreaView>
         )
     }
